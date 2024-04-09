384c139148b262470bdc2c5c2e56600a801d19dd
Florian-DAUVERGNE <116449669+Florian-DAUVERGNE@users.noreply.github.com>
2024-04-09 20:31:56 +0200

    Update capteur_ultrasons_v2.vhd

--- capteur_ultrasons_v2.vhd
+++ capteur_ultrasons_v2.vhd
@@ -14,16 +14,17 @@ architecture sonar_arch of sonar is
     constant conversion: real := 0.000001715;
     signal traveledDistance:real := 0.0;
     signal seventSegment: std_logic_vector(6 downto 0);
-    constant ZERO : std_logic_vector(6 downto 0) := "1000000";
-    constant UN : std_logic_vector(6 downto 0) := "1111001";
-    constant DEUX : std_logic_vector(6 downto 0) := "0100100";
-    constant TROIS : std_logic_vector(6 downto 0) := "0110000";
-    constant QUATRE : std_logic_vector(6 downto 0) := "0011001";
-    constant CINQ : std_logic_vector(6 downto 0) := "0010010";
-    constant SIX : std_logic_vector(6 downto 0) := "0000010";
-    constant SEPT : std_logic_vector(6 downto 0) := "1111000";
-    constant HUIT : std_logic_vector(6 downto 0) := "0000000";
-    constant NEUF : std_logic_vector(6 downto 0) := "0010000";
+    constant ZERO : std_logic_vector(6 downto 0) := "1111110";
+    constant UN : std_logic_vector(6 downto 0) := "0000110";
+    constant DEUX : std_logic_vector(6 downto 0) := "1011011";
+    constant TROIS : std_logic_vector(6 downto 0) := "1001111";
+    constant QUATRE : std_logic_vector(6 downto 0) :=  "0100111";
+    constant CINQ : std_logic_vector(6 downto 0) := "1101101";
+    constant SIX : std_logic_vector(6 downto 0) := "1111101";
+    constant SEPT : std_logic_vector(6 downto 0) := "1000110";
+    constant HUIT : std_logic_vector(6 downto 0) := "1111111";
+    constant NEUF : std_logic_vector(6 downto 0) :=  "1101111";
+    constant ERROR : std_logic_vector(6 downto 0) :=  "1111001";
 begin
     process(clk)
     begin
@@ -32,9 +33,8 @@ begin
 		receiveEcho <= '1';
 	end if;
 	if receiveEcho = '1' then
-		sendTrig <= '0';
 		traveledDistance <= real(count) * conversion;
-		count <= 0;
+		receiveEcho <= '0';
 	end if;
 
 	if rising_edge(clk) then
@@ -43,16 +43,17 @@ begin
 		end if;
         end if; 
 	case integer(traveledDistance * 1000.0) is
-		when 0 to 1000 => seventSegment <= UN;
-		when 1001 to 2000 => seventSegment <= DEUX;
-		when 2001 to 3000 => seventSegment <= TROIS;
-		when 3001 to 4000 => seventSegment <= QUATRE;
-		when 4001 to 5000 => seventSegment <= CINQ;
-		when 5001 to 6000 => seventSegment <= SIX;
-		when 6001 to 7000 => seventSegment <= SEPT;
-		when 7001 to 8000 => seventSegment <= HUIT;
-		when 8001 to 9000 => seventSegment <= NEUF;
-		when others => seventSegment <= ZERO;
+		when 0 to 1000 => seventSegment <= ZERO;
+		when 1001 to 2000 => seventSegment <= UN;
+		when 2001 to 3000 => seventSegment <= DEUX;
+		when 3001 to 4000 => seventSegment <= TROIS;
+		when 4001 to 5000 => seventSegment <= QUATRE;
+		when 5001 to 6000 => seventSegment <= CINQ;
+		when 6001 to 7000 => seventSegment <= SIX;
+		when 7001 to 8000 => seventSegment <= SEPT;
+		when 8001 to 9000 => seventSegment <= HUIT;
+		when 9001 to 10000 => seventSegment <= NEUF;
+		when others => seventSegment <= ERROR;
 	end case;      
     end process;
 end sonar_arch;
